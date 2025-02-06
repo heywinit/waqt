fc1cddd21f1feab0092a64e716858cad5883cd28
heywinit <heywinit@gmail.com>
2025-02-06 22:08:47 +0530

    🐛 FIX: minor setting changes

--- waqt-backend/internal/models/user.go
+++ waqt-backend/internal/models/user.go
@@ -1,12 +1,14 @@
 package models
 
 import (
+	"time"
+
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	BaseModel      // Embedding the BaseModel for common fields
+	BaseModel     
 	
 	Email          string `json:"email" gorm:"unique;not null"`
 	Password       string `json:"-" gorm:"not null"`
@@ -14,10 +16,35 @@ type User struct {
 	LastName       string `json:"last_name"`
 	ProfilePicture string `json:"profile_picture"`
 
+	GoogleID       *string   `json:"google_id,omitempty" gorm:"unique"`
+	EmailVerified  bool      `json:"email_verified" gorm:"default:false"`
+	LastLogin      time.Time `json:"last_login"`
+
 	// Relations
 	Tasks     []Task     `json:"tasks,omitempty" gorm:"foreignKey:UserID"`
 	Schedules []Schedule `json:"schedules,omitempty" gorm:"foreignKey:UserID"`
+	Settings  Settings   `json:"settings,omitempty" gorm:"foreignKey:UserID"`
 }
+
+type Settings struct {
+	BaseModel
+
+	ThemePreference             string    `json:"theme_preference"`
+	NotificationLevel           string    `json:"notification_level"`
+	Language                    string    `json:"language"`
+	DefaultTaskDuration         time.Time `json:"default_task_duration"`
+	TaskReminderIntervalMinutes int       `json:"task_reminder_interval_minutes"`
+	
+	Timezone                    string    `json:"timezone"`
+	DateFormat   string `json:"date_format"`
+	TimeFormat24 bool   `json:"time_format_24"`
+	
+	// Preferences
+	WorkStartTime string `json:"work_start_time" gorm:"default:'09:00'"`
+	WorkEndTime   string `json:"work_end_time" gorm:"default:'17:00'"`
+	WorkDays      string `json:"work_days" gorm:"default:'1,2,3,4,5'"` // 0=Sunday, 1=Monday, etc.
+}
+
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
